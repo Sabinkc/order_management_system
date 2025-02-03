20874bc59a9732683b7616eb0fc00e58fd90430b
Sabinkc <sabinkc1206@gmail.com>
2025-02-03 12:12:27 +0545

    adding to order history issue solved

--- lib/features/dashboard/presentation/widgets/checkout_widget.dart
+++ lib/features/dashboard/presentation/widgets/checkout_widget.dart
@@ -1,184 +1,9 @@
-// import 'package:flutter/material.dart';
-// import 'package:order_management_system/common/common_color.dart';
-// import 'package:order_management_system/features/dashboard/domain/cart_quantity_provider.dart';
-// import 'package:provider/provider.dart';
 
-// class CheckoutWidget extends StatelessWidget {
-//   const CheckoutWidget({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     final double screenWidth = MediaQuery.of(context).size.width;
-//     return Padding(
-//       padding: EdgeInsets.symmetric(horizontal: 20),
-//       child: Row(
-//         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//         crossAxisAlignment: CrossAxisAlignment.center,
-//         children: [
-//           Column(
-//             children: [
-//               Text(
-//                 "Total Price:",
-//                 style: TextStyle(fontWeight: FontWeight.bold, fontSize: 12),
-//               ),
-//               Consumer<CartQuantityProvider>(
-//                 builder: (context, provider, child) {
-//                   return RichText(
-//                       text: TextSpan(children: [
-//                     TextSpan(
-//                       text: "Rs.",
-//                       style: TextStyle(
-//                           fontWeight: FontWeight.bold,
-//                           color: CommonColor.primaryColor),
-//                     ),
-//                     TextSpan(
-//                       text: provider.getTotalPrice().toString(),
-//                       style: TextStyle(
-//                           fontSize: 20,
-//                           fontWeight: FontWeight.bold,
-//                           color: CommonColor.primaryColor),
-//                     ),
-//                   ]));
-//                 },
-//               ),
-//             ],
-//           ),
-//           SizedBox(
-//             width: screenWidth * 0.35,
-//             child: ElevatedButton(
-//                 style: ElevatedButton.styleFrom(
-//                   shape: RoundedRectangleBorder(
-//                       borderRadius: BorderRadius.circular(8)),
-//                   backgroundColor: CommonColor.primaryColor,
-//                 ),
-//                 onPressed: () {
-//                   showLogoutDialogAndCheckout(context);
-//                 },
-//                 child: Text(
-//                   "Check Out",
-//                   style: TextStyle(
-//                     color: Colors.white,
-//                     fontWeight: FontWeight.bold,
-//                   ),
-//                 )),
-//           )
-//         ],
-//       ),
-//     );
-//   }
-
-//   showLogoutDialogAndCheckout(BuildContext context) {
-//     showDialog(
-//         context: context,
-//         builder: (context) {
-//           return AlertDialog(
-//               shape: RoundedRectangleBorder(
-//                   borderRadius: BorderRadius.circular(15)),
-//               backgroundColor: Colors.white,
-//               alignment: Alignment.center,
-//               titlePadding: EdgeInsets.only(
-//                 top: 12,
-//                 bottom: 10,
-//               ),
-//               contentPadding: EdgeInsets.only(bottom: 12),
-//               title: Text(
-//                 "Confirm to checkout?",
-//                 textAlign: TextAlign.center,
-//                 style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
-//               ),
-//               content: Column(
-//                 mainAxisSize: MainAxisSize.min,
-//                 spacing: 10,
-//                 crossAxisAlignment: CrossAxisAlignment.center,
-//                 children: [
-//                   Padding(
-//                     padding: EdgeInsets.symmetric(horizontal: 10),
-//                     child: Text(
-//                       "Are you sure you want to checkout?",
-//                       textAlign: TextAlign.center,
-//                       style: TextStyle(
-//                           fontSize: 13, color: CommonColor.darkGreyColor),
-//                     ),
-//                   ),
-//                   Row(
-//                     mainAxisAlignment: MainAxisAlignment.center,
-//                     spacing: 15,
-//                     children: [
-//                       SizedBox(
-//                         width: 122,
-//                         child: ElevatedButton(
-//                             style: ElevatedButton.styleFrom(
-//                               shape: RoundedRectangleBorder(
-//                                   side: BorderSide(
-//                                     color: CommonColor.primaryColor,
-//                                   ),
-//                                   borderRadius: BorderRadius.circular(8)),
-//                               backgroundColor: Colors.white,
-//                             ),
-//                             onPressed: () {
-//                               Navigator.pop(context);
-//                             },
-//                             child: Text(
-//                               "Cancel",
-//                               style: TextStyle(color: CommonColor.primaryColor),
-//                             )),
-//                       ),
-//                       SizedBox(
-//                         width: 122,
-//                         child: ElevatedButton(
-//                             style: ElevatedButton.styleFrom(
-//                               shape: RoundedRectangleBorder(
-//                                   borderRadius: BorderRadius.circular(8)),
-//                               backgroundColor: CommonColor.primaryColor,
-//                             ),
-//                             onPressed: () {
-//                               checkout(context);
-//                             },
-//                             child: Text(
-//                               "Checkout",
-//                               style: TextStyle(color: Colors.white),
-//                             )),
-//                       ),
-//                     ],
-//                   )
-//                 ],
-//               ));
-//         });
-//   }
-
-//   checkout(BuildContext context) {
-//     Provider.of<CartQuantityProvider>(context, listen: false).clearCart();
-//     showDialog(
-//         context: context,
-//         builder: (context) {
-//           Future.delayed(Duration(seconds: 1), () {
-//             if (context.mounted) {
-//               Navigator.pop(context);
-//               Navigator.pop(context);
-//             }
-//           });
-//           return AlertDialog(
-//             backgroundColor: Colors.white,
-//             shape:
-//                 RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-//             title: SizedBox(
-//               height: 90,
-//               child: Center(
-//                 child: Text(
-//                   "Checkout completed!",
-//                   style:
-//                       TextStyle(color: CommonColor.darkGreyColor, fontSize: 14),
-//                 ),
-//               ),
-//             ),
-//           );
-//         });
-//   }
-// }
 
 import 'package:flutter/material.dart';
 import 'package:logger/logger.dart';
 import 'package:order_management_system/common/common_color.dart';
+import 'package:order_management_system/features/dashboard/data/cart_model.dart';
 import 'package:order_management_system/features/dashboard/domain/cart_quantity_provider.dart';
 import 'package:order_management_system/features/order%20history/domain/order_history_provider.dart';
 import 'package:provider/provider.dart';
@@ -326,50 +151,57 @@ class CheckoutWidget extends StatelessWidget {
         });
   }
 
-  checkout(BuildContext context) {
+
+  void checkout(BuildContext context) {
     final cartQuantityProvider =
         Provider.of<CartQuantityProvider>(context, listen: false);
     final orderHistoryProvider =
         Provider.of<OrderHistoryProvider>(context, listen: false);
-
+    final Logger logger = Logger();
     // Add the cart items to the order history
-    orderHistoryProvider.addOrder(cartQuantityProvider.cartItems);
-   
+    logger.i(
+        "Cart Items before adding to order history: ${cartQuantityProvider.cartItems}");
+    // Create a deep copy of the cart items
+    final List<CartModel> cartItemsCopy =
+        List.from(cartQuantityProvider.cartItems);
 
-
+    orderHistoryProvider.addOrder(cartItemsCopy);
+    logger.i(
+        "Order Items after adding to order history: ${orderHistoryProvider.orders}");
 
     // Log the cart items
-    final Logger logger = Logger();
-  
+
     logger.i("Cart Items before checkout: ${cartQuantityProvider.cartItems}");
-    logger.i("Order Items befor checkout: ${orderHistoryProvider.orders}");
+    logger.i("Order Items before checkout: ${orderHistoryProvider.orders}");
+
     // Clear the cart
     cartQuantityProvider.clearCart();
 
     showDialog(
-        context: context,
-        builder: (context) {
-          Future.delayed(Duration(seconds: 1), () {
-            if (context.mounted) {
-              Navigator.pop(context);
-              Navigator.pop(context);
-            }
-          });
-          return AlertDialog(
-            backgroundColor: Colors.white,
-            shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-            title: SizedBox(
-              height: 90,
-              child: Center(
-                child: Text(
-                  "Checkout completed!",
-                  style:
-                      TextStyle(color: CommonColor.darkGreyColor, fontSize: 14),
-                ),
+      context: context,
+      builder: (context) {
+        Future.delayed(Duration(seconds: 1), () {
+          if (context.mounted) {
+            Navigator.pop(context);
+            Navigator.pop(context);
+          }
+        });
+        return AlertDialog(
+          backgroundColor: Colors.white,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+          title: SizedBox(
+            height: 90,
+            child: Center(
+              child: Text(
+                "Checkout completed!",
+                style:
+                    TextStyle(color: CommonColor.darkGreyColor, fontSize: 14),
               ),
             ),
-          );
-        });
+          ),
+        );
+      },
+    );
   }
 }
--- lib/features/order history/presentation/screens/order_history_screen.dart
+++ lib/features/order history/presentation/screens/order_history_screen.dart
@@ -1,189 +1,10 @@
-// import 'package:flutter/material.dart';
-// import 'package:icons_plus/icons_plus.dart';
-// import 'package:intl/intl.dart';
-// import 'package:logger/logger.dart';
-// import 'package:order_management_system/common/common_color.dart';
-// import 'package:order_management_system/features/dashboard/domain/cart_quantity_provider.dart';
 
-// import 'package:order_management_system/features/order%20history/domain/order_history_provider.dart';
-// import 'package:provider/provider.dart';
-
-// class OrderHistoryScreen extends StatelessWidget {
-//   const OrderHistoryScreen({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     final screenHeight = MediaQuery.of(context).size.height;
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: RichText(
-//           text: TextSpan(
-//             children: [
-//               TextSpan(
-//                 text: "Ord",
-//                 style: TextStyle(
-//                   fontSize: 22,
-//                   color: CommonColor.primaryColor,
-//                   fontWeight: FontWeight.bold,
-//                 ),
-//               ),
-//               TextSpan(
-//                 text: "ers",
-//                 style: TextStyle(
-//                   fontSize: 22,
-//                   color: Colors.black,
-//                   fontWeight: FontWeight.bold,
-//                 ),
-//               ),
-//             ],
-//           ),
-//         ),
-//         centerTitle: true,
-//         automaticallyImplyLeading: false,
-//         actions: [
-//           Padding(
-//             padding: EdgeInsets.symmetric(horizontal: 15),
-//             child: IconButton(
-//               onPressed: () {
-//                 final orderHistoryProvider =
-//                     Provider.of<OrderHistoryProvider>(context, listen: false);
-//                 orderHistoryProvider.deleteOrders();
-//                 final Logger logger = Logger();
-//                 logger.i(orderHistoryProvider.orders.length);
-//               },
-//               icon: Icon(
-//                 MingCute.delete_2_fill,
-//                 size: 20,
-//               ),
-//             ),
-//           ),
-//         ],
-//       ),
-//       body: Padding(
-//         padding: EdgeInsets.symmetric(horizontal: 20),
-//         child: Consumer<OrderHistoryProvider>(
-//           builder: (context, provider, child) {
-//             // Check if orders are null or empty
-//             if (provider.orders.isEmpty) {
-//               return Center(
-//                 child: Text(
-//                   "No orders till now!",
-//                   style: TextStyle(fontSize: 20),
-//                 ),
-//               );
-//             }
-
-//             return ListView(
-//               children: [
-//                 Row(
-//                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//                   children: [
-//                     Text(
-//                       "Order History",
-//                       style: TextStyle(
-//                         fontWeight: FontWeight.bold,
-//                         fontSize: 16,
-//                       ),
-//                     ),
-//                     Row(
-//                       children: [
-//                         Text(
-//                           "Sort",
-//                           style: TextStyle(
-//                             color: CommonColor.mediumGreyColor,
-//                             fontWeight: FontWeight.bold,
-//                           ),
-//                         ),
-//                         Icon(
-//                           Icons.arrow_drop_down_outlined,
-//                           color: CommonColor.mediumGreyColor,
-//                         ),
-//                       ],
-//                     ),
-//                   ],
-//                 ),
-//                 SizedBox(height: screenHeight * 0.02),
-//                 ListView.builder(
-//                   shrinkWrap: true,
-//                   physics:
-//                       NeverScrollableScrollPhysics(), // Prevent inner ListView from scrolling
-//                   itemCount: provider.orders.length,
-//                   itemBuilder: (context, orderindex) {
-//                     final order = provider.orders[orderindex];
-
-//                     return Column(
-//                       crossAxisAlignment: CrossAxisAlignment.start,
-//                       children: [
-//                         // Display formatted date
-//                         Text(
-//                           DateFormat('yyyy-MM-dd')
-//                               .format(order["date"] as DateTime),
-//                           style: TextStyle(
-//                             fontWeight: FontWeight.bold,
-//                             fontSize: 14,
-//                           ),
-//                         ),
-//                         Container(
-//                           height: 200,
-//                           color: Colors.red,
-//                           child: ListView.builder(
-//                             shrinkWrap: true,
-//                             itemCount: 1,
-//                             itemBuilder: (context, itemIndex) {
-//                               return TextButton(
-//                                   onPressed: () {
-//                                     final Logger logger = Logger();
-//                                     logger.i("status: ${order["status"]}");
-//                                     logger.i(
-//                                         "Items length: ${order["items"].length}");
-//                                     logger.i(order["items"].isEmpty
-//                                         ? "No Product"
-//                                         : order["items"][0].productName);
-//                                     final cartQuantityProvider =
-//                                         Provider.of<CartQuantityProvider>(
-//                                             context,
-//                                             listen: false);
-//                                     final orderHistoryProvider =
-//                                         Provider.of<OrderHistoryProvider>(
-//                                             context,
-//                                             listen: false);
-//                                     logger.i(
-//                                         "cart items: ${cartQuantityProvider.cartItems}");
-//                                     logger.i(
-//                                         "orders: ${orderHistoryProvider.orders}");
-//                                         logger.i("OrderHistoryScreen orders: ${provider.orders}");
-
-//                                   },
-//                                   child: Text("Display"));
-//                             },
-//                           ),
-//                         ),
-//                         // Text("${order["items"][0]["productName"]}"),
-//                         SizedBox(height: 10),
-//                         // Display items in the order
-
-//                         Divider(
-//                             color: Colors.grey), // Add a divider between orders
-//                       ],
-//                     );
-//                   },
-//                 ),
-//               ],
-//             );
-//           },
-//         ),
-//       ),
-//     );
-//   }
-// }
 
 import 'package:flutter/material.dart';
 import 'package:icons_plus/icons_plus.dart';
 import 'package:intl/intl.dart';
 import 'package:logger/logger.dart';
 import 'package:order_management_system/common/common_color.dart';
-import 'package:order_management_system/features/dashboard/domain/cart_quantity_provider.dart';
-
 import 'package:order_management_system/features/order%20history/domain/order_history_provider.dart';
 import 'package:provider/provider.dart';
 
@@ -302,45 +123,21 @@ class OrderHistoryScreen extends StatelessWidget {
                             fontSize: 14,
                           ),
                         ),
-                        Container(
-                          height: 200,
-                          color: Colors.red,
-                          child: ListView.builder(
-                            shrinkWrap: true,
-                            itemCount: 1,
-                            itemBuilder: (context, itemIndex) {
-                              return TextButton(
-                                  onPressed: () {
-                                    final Logger logger = Logger();
-                                    logger.i("status: ${order["status"]}");
-                                    logger.i(
-                                        "Items length: ${order["items"].length}");
-                                    logger.i(order["items"].isEmpty
-                                        ? "No Product"
-                                        : order["items"][0].productName);
-                                    final cartQuantityProvider =
-                                        Provider.of<CartQuantityProvider>(
-                                            context,
-                                            listen: false);
-                                    final orderHistoryProvider =
-                                        Provider.of<OrderHistoryProvider>(
-                                            context,
-                                            listen: false);
-                                    logger.i(
-                                        "cart items: ${cartQuantityProvider.cartItems}");
-                                    logger.i(
-                                        "orders: ${orderHistoryProvider.orders}");
-                                    logger.i(
-                                        "OrderHistoryScreen orders: ${provider.orders}");
-                                  },
-                                  child: Text("Display"));
-                            },
-                          ),
-                        ),
-                        // Text("${order["items"][0]["productName"]}"),
                         SizedBox(height: 10),
-                        // Display items in the order
-
+                        // Display product names
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: order["items"]
+                              .map<Widget>((item) => Text(
+                                    item.productName,
+                                    style: TextStyle(
+                                      fontSize: 14,
+                                      color: Colors.black,
+                                    ),
+                                  ))
+                              .toList(),
+                        ),
+                        SizedBox(height: 10),
                         Divider(
                             color: Colors.grey), // Add a divider between orders
                       ],
